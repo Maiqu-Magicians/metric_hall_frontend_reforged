9fd9600e7bfdb510f08b60cfc9369e497648ecde
Dustella <fdnoaivj@outlook.com>
2022-04-28 20:26:17 +0800

    disable vconsole

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import router from './router'
 import { createPinia } from 'pinia'
 import VConsole from 'vconsole'
 import 'animate.css';
-const vConsole = new VConsole();
+// const vConsole = new VConsole();
 import 'element-plus/dist/index.css'
 createApp(App).use(createPinia()).use(router).mount('#app')

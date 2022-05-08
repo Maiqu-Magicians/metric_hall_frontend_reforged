2f2ef09d396ade5423713b2124e5122afa55fd37
Dustella <fdnoaivj@outlook.com>
2022-05-08 16:39:54 +0800

    产品打开动画的测试

--- src/Main.vue
+++ src/Main.vue
@@ -3,31 +3,32 @@
     <main-footer/>
     <router-view/>
   </div>
-
 </template>
 
 <script lang="ts" setup>
 import MainFooter from "./components/Base/Footer.vue"
 import {loginState} from "./store/loginStatus";
-import {onMounted, onBeforeMount} from "vue";
+import {onBeforeMount} from "vue";
 import {userInfo} from "./store/userInfo";
 import {ProductStore} from "./store/products";
 import axios from "axios";
+
 // begin init login state
-const login = loginState()
+const login = loginState();
 const info = userInfo();
 const products = ProductStore();
+
 axios.defaults.baseURL="https://api.maiquer.tech"
 
 
 onBeforeMount(async () => {
 //after mounted, init products
       login.loadfromLocal();
+      await products.getAll();
       if (login.isLoggedIn) {
         axios.defaults.headers.common["Authorization"] = login.jwtToken
         await info.fetchInfo(login.userid)
       }
-      await products.getAll();
     }
 )
 
